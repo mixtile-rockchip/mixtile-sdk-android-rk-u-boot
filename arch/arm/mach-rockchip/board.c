765ff0ffdf818245db175f68e19677af4f302534
evtest-hash <84323112+evtest-hash@users.noreply.github.com>
2026-01-22 17:27:28 +0800

    rockchip: show U-Boot logo earlier in board_late_init
    Move rockchip_show_logo() before download mode handling and
    command execution so the logo is displayed earlier during
    U-Boot boot.

--- arch/arm/mach-rockchip/board.c
+++ arch/arm/mach-rockchip/board.c
@@ -488,6 +488,11 @@ int board_late_init(void)
 #ifdef CONFIG_ROCKCHIP_SET_SN
 	rockchip_set_serialno();
 #endif
+#ifdef CONFIG_DRM_ROCKCHIP
+	if ((rockchip_get_boot_mode() != BOOT_MODE_QUIESCENT) &&
+	     !smp_event1(SEVT_3, STID_16))
+		rockchip_show_logo();
+#endif
 	setup_download_mode();
 #ifdef CONFIG_ROCKCHIP_USB_BOOT
 	boot_from_udisk();
@@ -500,11 +505,7 @@ int board_late_init(void)
 	rk_minidump_init();
 #endif
 
-#ifdef CONFIG_DRM_ROCKCHIP
-	if ((rockchip_get_boot_mode() != BOOT_MODE_QUIESCENT) &&
-	     !smp_event1(SEVT_3, STID_16))
-		rockchip_show_logo();
-#endif
+
 
 #ifdef CONFIG_ROCKCHIP_EBOOK_DISPLAY
 	rockchip_ebook_show_uboot_logo();
